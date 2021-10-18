3b1aae6904faf05f7e4f76154cd896ea97a86c0b
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-10-19 01:28:17 +0800

    Update Q6-1018.c

--- Q6-1018.c
+++ Q6-1018.c
@@ -2,13 +2,12 @@
 
 int main(void){
     double n;
-    double total;
+    double total=0;
     scanf("%lf",&n);
     if (n>0)
     {
         for (double i = 1; i <= n ; i++)
         {
-            total = 0;
             total += 1/((2*i-1)*2*i);
         }
         printf("%.16lf", total);
