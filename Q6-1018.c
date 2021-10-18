d0b9f09f9366915248ec9da180eca8211247a5b6
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-10-19 00:49:47 +0800

    Add files via upload

---
+++ Q6-1018.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void){
+    double n;
+    double total;
+    scanf("%lf",&n);
+    if (n>0)
+    {
+        for (double i = 1; i <= n ; i++)
+        {
+            total += 1/((2*i-1)*2*i);
+        }
+        printf("%.16lf", total);
+    }
+    
+
+}
